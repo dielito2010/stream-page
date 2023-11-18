94db3be29dd7c7029766f6c5be573a7c82423215
dielito2010 <d.a.ribeiro2@hotmail.com>
2023-11-17 20:01:00 -0400

    config vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/stream-page-example",
+  base: "/stream-page",
   plugins: [react()],
-  server: {
-    port: 3355,
-  },
+  // server: {
+  //   port: 3355,
+  // },
 });
